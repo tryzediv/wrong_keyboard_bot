17dfb17512de32c498a71c44afa83e6da276d148
Dmitry <122273371+tryzediv@users.noreply.github.com>
2023-03-10 17:35:11 +0400

    Add files via upload

---
+++ wrong_keyboard_bot.py
@@ -0,0 +1,53 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('TOKEN')
+
+
+# Функция по первому символу определяет на какой язык нужно перевести сообщение
+# и переводит согласно раскладке клавиатуры
+def wrong_key(text):
+    en_keys = "`qwertyuiop[]asdfghjkl;'zxcvbnm,."
+    ru_keys = "ёйцукенгшщзхъфывапролджэячсмитьбю"
+    result = ''
+
+    if text[0].lower() in en_keys:
+        for i in text.lower():
+            if i in en_keys:
+                result += ru_keys[en_keys.index(i)]
+            else:
+                result += i
+    else:
+        for i in text.lower():
+            if i in ru_keys:
+                result += en_keys[ru_keys.index(i)]
+            else:
+                result += i
+    return result.capitalize()
+
+
+# Стартовое, приветственное сообщение от бота
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+    bot.send_message(message.chat.id, "Если вы ошиблись раскладкой клавиатуры, "
+                                      "присылайте свой текст сюда, я все исправлю ;)")
+
+
+# Ответ на сообщение, возвращает сообщение в правильной раскладке
+@bot.message_handler(content_types=['text'])
+def echo(message):
+    bot.send_message(message.chat.id, wrong_key(message.text))
+
+
+# Режим вызова бота в чатах
+@bot.inline_handler(func=lambda query: len(query.query) > 0)
+def query_text(query):
+    r = types.InlineQueryResultArticle(
+        id='1', title="Ошиблись раскладкой?",
+        description='Вставьте ваше сообщение сюда, я все починю =)',
+        input_message_content=types.InputTextMessageContent(
+            message_text=f'{wrong_key(query.query)}'))
+    bot.answer_inline_query(query.id, [r])
+
+
+bot.polling(none_stop=True)
