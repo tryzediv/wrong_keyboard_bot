ef757c61e57d26f2950cc9374a7252a4c0c9594a
d.e.kharin <d.e.kharin@tmn3.etagi.com>
2024-02-16 12:51:32 +0500

    Add unittests

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
-from dnd_bot import diff_lvl_func, diff_lvl, dice_throw
+from dice_bot.dnd_bot import diff_lvl_func, diff_lvl, dice_throw
+from wrong_keyboard.wrong_keyboard_bot import wrong_key
 
 
 class DndTestCases(unittest.TestCase):
@@ -18,6 +19,18 @@ class DndTestCases(unittest.TestCase):
         print('Run test_dice_throw')
         mess_1, mess_2 = dice_throw(1)
         self.assertIn('Вам выпало', mess_1, 'Ошибка в функции dice_throw')
+
+    def test_wrong_key(self):
+        print('Run test_wrong_key')
+        result = wrong_key("`qwertyuiop[]asdfghjkl;'zxcvbnm,./?")
+        self.assertEqual(result, "Ёйцукенгшщзхъфывапролджэячсмитьбю.,",
+                         'Ошибка при конвертировании английской раскладки в русскую')
+
+    def test_wrong_key_2(self):
+        print('Run test_wrong_key_2')
+        result = wrong_key("ёйцукенгшщзхъфывапролджэячсмитьбю.,")
+        self.assertEqual(result, "`qwertyuiop[]asdfghjkl;'zxcvbnm,./?",
+                         'Ошибка при конвертировании русской раскладки в английскую')
 
 
 if __name__ == '__main__':
